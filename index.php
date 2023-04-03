b78398b5a894b1684716902f505e3dfaa9cb754c
Juan  Hernandez <72632179+juan3232@users.noreply.github.com>
2023-04-02 22:32:51 -0600

    guardar

---
+++ index.php
@@ -0,0 +1,5 @@
+git add * -> agrega los cambios al stash
+git commit -m "descripcion del cambio" -> acomete los cambios
+git push  -> sube los cambios al remoto
+git fetch -> actualiza el remoto para saber si hubo cambios comparado a tu local
+git pull -> descarga los cambios del remoto
